3c691c386207a5e5d716121fa7cf3527d1a3d99c
Maxim Fateev <mfateev@gmail.com>
2025-12-30 09:49:31 -0800

    Add Worker extensions for Kotlin coroutine workflows
    - registerKotlinWorkflowImplementationTypes(plugin, vararg KClass<*>)
      auto-detects suspend workflows using KotlinWorkflowDefinition.isSuspendWorkflow()
    - Routes suspend workflows to KotlinWorkflowImplementationFactory
    - Falls back to Java factory for non-suspend workflows
    - Convenience overload with default KotlinPlugin
    - Reified generic methods for single-type registration

--- temporal-kotlin/src/main/kotlin/io/temporal/worker/WorkerExt.kt
+++ temporal-kotlin/src/main/kotlin/io/temporal/worker/WorkerExt.kt
@@ -1,7 +1,13 @@
+@file:OptIn(io.temporal.kotlin.internal.InternalTemporalApi::class)
 
 package io.temporal.worker
 
+import io.temporal.common.converter.DataConverter
 import io.temporal.kotlin.TemporalDsl
+import io.temporal.kotlin.internal.KotlinWorkflowDefinition
+import io.temporal.kotlin.internal.KotlinWorkflowImplementationFactory
+import io.temporal.kotlin.worker.KotlinPlugin
+import kotlin.reflect.KClass
 
 /**
  * Registers workflow implementation classes with a worker.
@@ -118,4 +124,110 @@ inline fun <reified T : Any> Worker.registerWorkflowImplementationFactory(
   noinline factory: () -> T
 ) {
   registerWorkflowImplementationFactory(T::class.java, factory)
+}
+
+// ==================== Kotlin Coroutine Workflow Registration ====================
+
+/**
+ * Registers Kotlin workflow implementation classes with automatic detection of suspend functions.
+ *
+ * This extension automatically routes workflow implementations to the appropriate factory:
+ * - Suspend function workflows are registered with [KotlinWorkflowImplementationFactory]
+ * - Non-suspend workflows are registered with the standard Java SDK factory
+ *
+ * Example:
+ * ```kotlin
+ * val plugin = KotlinPlugin()
+ * worker.registerKotlinWorkflowImplementationTypes(
+ *   plugin,
+ *   MyWorkflowImpl::class,
+ *   AnotherWorkflowImpl::class
+ * )
+ * ```
+ *
+ * @param plugin the Kotlin plugin providing configuration for coroutine workflows
+ * @param workflowImplementationClasses the workflow implementation classes to register
+ * @throws IllegalArgumentException if a class doesn't implement a valid workflow interface
+ * @see KotlinPlugin
+ */
+fun Worker.registerKotlinWorkflowImplementationTypes(
+  plugin: KotlinPlugin,
+  vararg workflowImplementationClasses: KClass<*>
+) {
+  val suspendWorkflows = mutableListOf<Class<*>>()
+  val javaWorkflows = mutableListOf<Class<*>>()
+
+  // Partition workflows by whether they use suspend functions
+  for (kClass in workflowImplementationClasses) {
+    val javaClass = kClass.java
+    if (KotlinWorkflowDefinition.isSuspendWorkflow(javaClass)) {
+      suspendWorkflows.add(javaClass)
+    } else {
+      javaWorkflows.add(javaClass)
+    }
+  }
+
+  // Register suspend workflows with Kotlin factory
+  if (suspendWorkflows.isNotEmpty()) {
+    val factory = plugin.createFactory(DataConverter.getDefaultInstance())
+    suspendWorkflows.forEach { factory.registerWorkflowImplementationType(it) }
+    registerWorkflowImplementationFactory(factory)
+  }
+
+  // Register non-suspend workflows with standard Java factory
+  if (javaWorkflows.isNotEmpty()) {
+    registerWorkflowImplementationTypes(*javaWorkflows.toTypedArray())
+  }
+}
+
+/**
+ * Registers Kotlin workflow implementation classes with a default [KotlinPlugin].
+ *
+ * This is a convenience overload that uses default plugin options.
+ *
+ * Example:
+ * ```kotlin
+ * worker.registerKotlinWorkflowImplementationTypes(
+ *   MyWorkflowImpl::class,
+ *   AnotherWorkflowImpl::class
+ * )
+ * ```
+ *
+ * @param workflowImplementationClasses the workflow implementation classes to register
+ * @throws IllegalArgumentException if a class doesn't implement a valid workflow interface
+ */
+fun Worker.registerKotlinWorkflowImplementationTypes(
+  vararg workflowImplementationClasses: KClass<*>
+) {
+  registerKotlinWorkflowImplementationTypes(KotlinPlugin(), *workflowImplementationClasses)
+}
+
+/**
+ * Registers a single Kotlin workflow implementation type.
+ *
+ * Example:
+ * ```kotlin
+ * worker.registerKotlinWorkflowImplementationType<MyWorkflowImpl>()
+ * ```
+ *
+ * @param T the workflow implementation type to register
+ */
+inline fun <reified T : Any> Worker.registerKotlinWorkflowImplementationType() {
+  registerKotlinWorkflowImplementationTypes(T::class)
+}
+
+/**
+ * Registers a single Kotlin workflow implementation type with a custom plugin.
+ *
+ * Example:
+ * ```kotlin
+ * val plugin = KotlinPlugin { deadlockDetectionTimeout = 2000L }
+ * worker.registerKotlinWorkflowImplementationType<MyWorkflowImpl>(plugin)
+ * ```
+ *
+ * @param T the workflow implementation type to register
+ * @param plugin the Kotlin plugin providing configuration
+ */
+inline fun <reified T : Any> Worker.registerKotlinWorkflowImplementationType(plugin: KotlinPlugin) {
+  registerKotlinWorkflowImplementationTypes(plugin, T::class)
 }
