c196e15480029807a4dfbed0f1fe56d8fdf911aa
Maxim Fateev <mfateev@gmail.com>
2026-01-07 09:54:28 -0800

    Add KOnConflictOptions data class for workflow start conflict handling
    Kotlin data class for OnConflictOptions with:
    - Constructor validation (attachRequestId required when attachCompletionCallbacks true)
    - toJavaOptions() for Java SDK interop

---
+++ temporal-kotlin/src/main/kotlin/io/temporal/kotlin/client/KOnConflictOptions.kt
@@ -0,0 +1,23 @@
+package io.temporal.kotlin.client
+
+import io.temporal.client.OnConflictOptions
+
+public data class KOnConflictOptions(
+    val attachRequestId: Boolean = false,
+    val attachCompletionCallbacks: Boolean = false,
+    val attachLinks: Boolean = false
+) {
+    init {
+        if (attachCompletionCallbacks) {
+            require(attachRequestId) {
+                "attachRequestId must be true if attachCompletionCallbacks is true"
+            }
+        }
+    }
+
+    public fun toJavaOptions(): OnConflictOptions = OnConflictOptions.newBuilder()
+        .setAttachRequestId(attachRequestId)
+        .setAttachCompletionCallbacks(attachCompletionCallbacks)
+        .setAttachLinks(attachLinks)
+        .build()
+}
