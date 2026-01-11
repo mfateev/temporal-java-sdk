ffc29105c6e1b7b3240a5307a276ad7074930923
Maxim Fateev <1463622+mfateev@users.noreply.github.com>
2026-01-11 13:53:40 -0800

    Restore preexisting deprecated methods in extension files
    WorkflowServiceStubsExt.kt and WorkerExt.kt are preexisting
    extension files from 2021-2022 that should remain unchanged.
    Restore deprecated methods that were incorrectly removed:
    
    - WorkflowServiceStubs() - deprecated since 2022
    - WorkflowServiceStubs(options) - deprecated since 2022
    - ConnectedWorkflowServiceStubs(options, timeout) - deprecated since 2022
    - Worker.addWorkflowImplementationFactory(options, factory) - deprecated
    - Worker.addWorkflowImplementationFactory(factory) - deprecated
    
    These deprecations follow Java SDK changes and should be
    maintained for backwards compatibility.

--- temporal-kotlin/src/main/kotlin/io/temporal/worker/WorkerExt.kt
+++ temporal-kotlin/src/main/kotlin/io/temporal/worker/WorkerExt.kt
@@ -1,5 +1,3 @@
-@file:OptIn(io.temporal.kotlin.internal.InternalTemporalApi::class)
-
 package io.temporal.worker
 
 import io.temporal.kotlin.TemporalDsl
@@ -43,6 +41,27 @@ inline fun <reified T : Any> Worker.registerWorkflowImplementationType(
 
 /**
  * Configures a factory to use when an instance of a workflow implementation is created.
+ *
+ * @param T Workflow interface that this factory implements
+ * @param factory factory that when called creates a new instance of the workflow implementation
+ * object.
+ * @see Worker.addWorkflowImplementationFactory
+ * @deprecated See deprecation notes on [Worker.addWorkflowImplementationFactory]
+ */
+@Deprecated(
+  "Use registerWorkflowImplementationFactory instead",
+  ReplaceWith("this.registerWorkflowImplementationFactory(options, factory)")
+)
+@Suppress("Deprecation")
+inline fun <reified T : Any> Worker.addWorkflowImplementationFactory(
+  options: WorkflowImplementationOptions,
+  noinline factory: () -> T
+) {
+  addWorkflowImplementationFactory(options, T::class.java, factory)
+}
+
+/**
+ * Configures a factory to use when an instance of a workflow implementation is created.
  * Please read an original [Worker.registerWorkflowImplementationFactory] method doc because
  * this method has a limited usage.
  *
@@ -57,6 +76,32 @@ inline fun <reified T : Any> Worker.registerWorkflowImplementationFactory(
   noinline factory: () -> T
 ) {
   registerWorkflowImplementationFactory(T::class.java, factory, options)
+}
+
+/**
+ * This method may behave differently from your expectations!
+ * Read deprecation and migration notes on [Worker.addWorkflowImplementationFactory].
+ * Configures a factory to use when an instance of a workflow implementation is created.
+ *
+ * ```kotlin
+ * worker.addWorkflowImplementationFactory<ChildWorkflow> {
+ *   val child = mock<ChildWorkflow>()
+ *   when(child.workflow(anyString(), anyString())).thenReturn("result1")
+ *   child
+ * }
+ * ```
+ *
+ * @param T Workflow interface that this factory implements
+ * @param factory factory that when called creates a new instance of the workflow implementation
+ * object.
+ * @see Worker.addWorkflowImplementationFactory
+ */
+@Deprecated("Use registerWorkflowImplementationFactory instead", ReplaceWith("this.registerWorkflowImplementationFactory(factory)"))
+@Suppress("Deprecation")
+inline fun <reified T : Any> Worker.addWorkflowImplementationFactory(
+  noinline factory: () -> T
+) {
+  addWorkflowImplementationFactory(T::class.java, factory)
 }
 
 /**
